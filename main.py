3d84fb8691a29f77f967012a8a844515ee64b01f
Vivek Katial <vivekkatial@gmail.com>
2021-06-16 15:44:25 +1000

    making change

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ args = parser.parse_args()
 
 totalPopulation = args.total_population[0]
 growthFactor = args.growth[0]
+
 dayCount = 0 #Every 2 months the population is reported
 total_days = 0
+
 while totalPopulation < 1000000:
     totalPopulation *= growthFactor
     #Every 56th day, population is reported
@@ -22,6 +24,6 @@ while totalPopulation < 1000000:
     total_days += 1
     if dayCount == 56: 
         dayCount = 0
-        print("Total Population: %s"%totalPopulation)
+        # print("Total Population: %s"%totalPopulation)
 
 print("Days to Reach 1M people:%s"%total_days)
